1995876c9fe27df2a267c6b9c565dea4a0a276cb
Lou Dace <lsdace30095@users.noreply.github.com>
2023-03-28 14:41:06 -0700

    Update entrypoint.sh
    updates --workers 8 --timeout 300

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ python manage.py collectstatic --no-input
 
 DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
 
-gunicorn core.wsgi:application --bind 0.0.0.0:8000 
+gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 8 --timeout 300
