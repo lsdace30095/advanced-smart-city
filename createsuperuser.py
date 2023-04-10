b143e39cf2e041fbaca99fa9e2d3a936f96c3402
Lou Dace <lsdace30095@users.noreply.github.com>
2023-04-10 00:55:09 +0000

    Update createsuperuser.py
    fixed username and password removing ""

--- createsuperuser.py
+++ createsuperuser.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 try:
     superuser = User.objects.create_superuser(
-        username="admin",
-        email="admin@example.com",
-        password="admin")
+        username=admin,
+        email=admin@dace-it.us,
+        password=admin)
     superuser.save()
 except IntegrityError:
     print(f"Super User with username admin is already exit!")
